35eb6fba2cc7691a9be2ac74a05f6c3582eecc92
[lishow] <[921079664@qq.com]>
2019-02-13 11:17:11 +0800

    Initial commit

---
+++ im2ascii.py
@@ -0,0 +1,62 @@
+# -*- coding=utf-8 -*-
+
+from PIL import Image
+import argparse
+
+# 命令行输入参数处理
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file')                            # 输入文件
+parser.add_argument('-o', '--output')                  # 输出文件
+parser.add_argument('--width', type=int, default=80)   # 输出字符画宽
+parser.add_argument('--height', type=int, default=60)  # 输出字符画高
+
+# 获取参数
+args = parser.parse_args()
+
+IMG = args.file
+WIDTH = args.width
+HEIGHT = args.height
+OUTPUT = args.output
+
+print("IMG is %s" % IMG)
+print("WIDTH is %d" % WIDTH)
+print("HEIGHT is %d" % HEIGHT)
+print("OUTPUT is %s" % OUTPUT)
+
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,")
+
+
+# 将256灰度映射到70个字符上
+def get_char(r, g, b, alpha=256):
+    if alpha == 0:
+        return ' '
+    length = len(ascii_char)
+    gray = int(2126 * r + 7152 * g + 722 * b)/10000
+
+    unit = (256.0 + 1)/length
+    return ascii_char[int(gray/unit)]
+
+
+if __name__ == '__main__':
+
+    rgb_im = Image.open(IMG)
+    im = rgb_im.convert('RGB')
+    im = im.resize((WIDTH, HEIGHT), Image.NEAREST)
+
+    txt = ""
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j,i)))
+        txt += '\n'
+
+    print(txt)
+
+    # 字符画输出到文件
+    if OUTPUT:
+        with open(OUTPUT, 'w') as f:
+            f.write(txt)
+    else:
+        with open("output.txt", 'w') as f:
+            f.write(txt)
